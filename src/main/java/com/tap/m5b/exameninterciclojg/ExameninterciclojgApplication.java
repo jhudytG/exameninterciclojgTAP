935deb460dcdf7e8fa22e78462189a5ff2259e27
Jhudyt Gaona <jhudyt.gaona.est@tecazuay.edu.ec>
2023-06-13 08:58:33 -0500

    .

---
+++ src/main/java/com/tap/m5b/exameninterciclojg/ExameninterciclojgApplication.java
@@ -0,0 +1,13 @@
+package com.tap.m5b.exameninterciclojg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameninterciclojgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameninterciclojgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tap/m5b/exameninterciclojg/ExameninterciclojgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap.m5b.exameninterciclojg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameninterciclojgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
